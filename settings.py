50fe2ea000e4b95564b78531a7b701364cf1d6d0
koji-kazuki <kazu.k0612@outlook.jp>
2024-12-11 00:23:41 +0900

    test

--- settings.py
+++ settings.py
@@ -1,10 +1,8 @@
 import os
 
-# モジュールの親ディレクトリのフルパスを取得
 basedir = os.path.dirname(os.path.dirname(__file__))
-# 親ディレクトリのpict.sqliteをデータベースに設定
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
-                                basedir, 'app.sqlite')
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///' + os.path.join(basedir,'app.sqlite')                                
 
 # シークレットキーの値として10バイトの文字列をランダムに生成
 SECRET_KEY = os.urandom(10)
