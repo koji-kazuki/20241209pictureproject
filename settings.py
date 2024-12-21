44616358483f6a6c9d3df15c5f8f02d28a92289f
koji-kazuki <kazu.k0612@outlook.jp>
2024-12-21 21:28:28 +0900

    test

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 
-basedir = os.path.dirname(__file__)
+basedir = Path(__file__).parent
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///' + os.path.join(basedir,'app.sqlite')                                
 
@@ -8,7 +9,7 @@ SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://",
 SECRET_KEY = os.urandom(10)
 
 # 画像のアップロード先のフォルダーを登録
-from pathlib import Path
+
 # basedirにapps、imagesを連結してPathオブジェクトを生成し、
 # str()で文字列に変換
 UPLOAD_FOLDER = str(Path(basedir,'images'))
